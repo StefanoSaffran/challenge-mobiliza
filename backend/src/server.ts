125a0ff57bb3004c7bcb9ee7f919b8870056efed
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-20 16:00:19 -0400

    :tada: Initial Structure

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+
+  return response.send('api rodando 🚀');
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Server started! Port: 3333!');
+});
