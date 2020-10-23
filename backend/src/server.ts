868d553c4681f3eb1acbca0cc52b9cf1c7bbe1e7
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-23 17:33:23 -0400

    :recycle: chore: add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import cors from 'cors';
 import routes from './routes';
 
 import './database/connection';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
