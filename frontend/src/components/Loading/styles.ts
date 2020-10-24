b393143bb1c7001a5500fc527f9b6954e3b81c17
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-24 08:31:28 -0400

    :sparkles: feat: create Loading component

---
+++ frontend/src/components/Loading/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > div {
+    svg g g path {
+      stroke: #fff;
+    }
+  }
+`;
